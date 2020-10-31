6a2e409a446557bc9382972bcd47e907a685e1f6
arnamen <aew11111d@gmail.com>
2020-10-31 17:17:29 +0200

    about-us styling fix

--- src/components/InformationPages/AboutUs/AboutUs.js
+++ src/components/InformationPages/AboutUs/AboutUs.js
@@ -9,15 +9,15 @@ export default function AboutUs() {
         <div className='about-us'>
             <h1 className='about-us__main-header'>О компании</h1>
             <hr/>
-            <h1>Заголовок 1 уровня</h1>
+            <h1 className='about-us__subtitle'>Заголовок 1 уровня</h1>
             <span>
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
             </span>
-            <h2>Заголовок 2 уровня</h2>
+            <h2 className='about-us__subtitle'>Заголовок 2 уровня</h2>
             <span>
             Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit
             </span>
-            <h3>Заголовок 3 уровня</h3>
+            <h3 className='about-us__subtitle'>Заголовок 3 уровня</h3>
             <span>
             Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, </span>
             <ul className='about-us__list'>
