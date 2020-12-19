8bb3479da43115841faef06bec150e9b03b5373e
arnamen <aew11111d@gmail.com>
2020-12-19 15:04:21 +0200

    account page: navbar added

--- src/components/ClientAccount/ClientAccount.js
+++ src/components/ClientAccount/ClientAccount.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 import './ClientAccount.css';
 
@@ -9,7 +10,27 @@ function ClientAccount() {
             <div className='ClientAccount__content'>
                 <h1>Содержимое</h1>
             </div>
-            <div className='ClientAccount__navigation'></div>
+            <div className='ClientAccount__navigation'>
+                <NavLink className='ClientAccount__navigation-link' 
+                isActive={(match, location) => {
+                    if(match || location.pathname === '/account') return true;
+                    return false;
+                }}
+                activeClassName='ClientAccount__navigation-link-active'
+                to='/account/orders'>История заказов</NavLink>
+                <NavLink className='ClientAccount__navigation-link' 
+                activeClassName='ClientAccount__navigation-link-active'
+                to='/account/delivery'>Адрес доставки</NavLink>
+                <NavLink className='ClientAccount__navigation-link' 
+                activeClassName='ClientAccount__navigation-link-active'
+                to='/account/discount'>Скидки и бонусы</NavLink>
+                <NavLink className='ClientAccount__navigation-link' 
+                activeClassName='ClientAccount__navigation-link-active'
+                to='/account/contacts'>Контактные данные</NavLink>
+                <NavLink className='ClientAccount__navigation-link' 
+                activeClassName='ClientAccount__navigation-link-active'
+                to='/account/loguot'>Выход</NavLink>
+            </div>
         </div>
     )
 }
