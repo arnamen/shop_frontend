1ff6e0a072d20b533e52329b1834f9843f81a030
arnamen <aew11111d@gmail.com>
2021-03-16 19:19:05 +0200

    CategoriesCards styles adjustments

--- src/components/UI/Cards/CategoriesCards/CategoriesCard/CategoriesCard.js
+++ src/components/UI/Cards/CategoriesCards/CategoriesCard/CategoriesCard.js
@@ -5,7 +5,8 @@ import classes from './CategoriesCard.module.css';
 
 export default function CategoriesCard(props) {
     return (
-        <Link href={props.to} className={classes.CategoriesCard}>
+        <Link href={props.to}>
+            <a className={classes.CategoriesCard}>
             <div className={classes.CategoriesCard_wrapper}>
                 <div className={classes['CategoriesCard_title-wrapper']}>
                     <span className={classes.CategoriesCard_title}>
@@ -14,6 +15,7 @@ export default function CategoriesCard(props) {
                 </div>
                 <img src={props.img} alt='category' className={classes.CategoriesCard_image}></img>
             </div>
+            </a>
         </Link>
     )
 }
